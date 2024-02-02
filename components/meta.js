ba44aa85a71db7f1276928b023f2b1aa2d992155
itc-n23029 <n23029@std.it-college.ac.jp>
2024-02-02 03:18:47 +0000

    chapter6.6finished

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import { siteMeta } from 'lib/constants'
+import { useRouter } from 'next/router'
+import siteImg from 'images/ogp.jpg'
 const {
   siteTitle,
   siteDesc,
@@ -9,15 +11,42 @@ const {
   siteIcon
 } = siteMeta
 
-const Meta = ({ pageTitle }) => {
-  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
+const Meta = props => {
+  const title = props.pageTitle
+    ? `${props.pageTitle} | ${siteTitle}`
+    : siteTitle
+  const desc = props.pageDesc ?? siteDesc
 
+  const router = useRouter()
+  const url = `${siteUrl}${router.asPath}`
+
+  // OGP画像
+  const img = props.Img || siteImg.src
+  const imgW = props.pageImgW || siteImg.width
+  const imgH = props.pageImgH || siteImg.height
+  const imgUrl = img.startsWith('https') ? img : `${siteUrl}${img}`
   return (
     <Head>
-      <title>
-        {pageTitle} | {siteTitle}
-      </title>
-      <meta property='og:title' content={pageTitle} />
+      <title>{title}</title>
+      <meta property='og:title' content={title} />
+
+      <meta name='description' content={desc} />
+      <meta property='og:description' content={desc} />
+
+      <link rel='canonical' href={url} />
+      <meta property='og:url' content={url} />
+
+      <meta property='og:site_name' content={siteTitle} />
+      <meta property='og:type' content={siteType} />
+      <meta property='og:locale' content={siteLocale} />
+
+      <link rel='icon' href={siteIcon} />
+      <link rel='apple-touch-icon' href={siteIcon} />
+
+      <meta property='og:image' content={imgUrl} />
+      <meta property='og:image:width' content={imgW} />
+      <meta property='og:image:height' content={imgH} />
+      <meta name='twitter:card' content='summary_large_image' />
     </Head>
   )
 }
